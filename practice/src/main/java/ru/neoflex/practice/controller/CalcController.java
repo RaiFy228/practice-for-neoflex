5eb480e6b34ed07b5d46bc57c0cb88373155cace
Владимир <118426381+RaiFy228@users.noreply.github.com>
2024-01-23 21:34:08 +0300

    Исправлены ошибки
    Исправлены ошибки взаимодействия БД и тестов

--- practice/src/main/java/ru/neoflex/practice/controller/CalcController.java
+++ practice/src/main/java/ru/neoflex/practice/controller/CalcController.java
@@ -1,6 +1,5 @@
 package ru.neoflex.practice.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.neoflex.practice.entity.Calculation;
 import ru.neoflex.practice.repository.CalculationRepository;
@@ -9,24 +8,24 @@ import java.util.List;
 
 @RestController
 public class CalcController {
-    private CalculationRepository calculationRepository;
-    private Calculation calculation;
-    private int result;
+    private final CalculationRepository calculationRepository;
+    //Внедрение репозитория в CalcController
+    public CalcController(CalculationRepository calculationRepository) {
+        this.calculationRepository = calculationRepository;
+    }
 
-    //Метод суммы
+    // Метод суммы
     @GetMapping("/plus/{a}/{b}")
     public int plus(@PathVariable("a") int a, @PathVariable("b") int b) {
-        result = a + b;
-        calculation = new Calculation(result);
-        calculationRepository.save(calculation);
+        int result = a + b;
+        calculationRepository.save(new Calculation(result));
         return result;
     }
-    //Метод разности
+    // Метод разности
     @GetMapping("/minus/{a}/{b}")
     public int minus(@PathVariable("a") int a, @PathVariable("b") int b) {
-        result = a - b;
-        calculation = new Calculation(result);
-        calculationRepository.save(calculation);
+        int result = a - b;
+        calculationRepository.save(new Calculation(result));
         return result;
     }
 
@@ -34,5 +33,4 @@ public class CalcController {
     public List <Calculation> getAllCalculations(){
         return calculationRepository.findAll();
     }
-
 }
